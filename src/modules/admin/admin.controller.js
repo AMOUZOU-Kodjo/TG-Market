c9812d70c1e44d28f9c403f6b0b926e9de17b674
Kodjo AMOUZOU <phipsipy@gmail.com>
2026-07-27 15:27:56 +0000

    fix: remove duplicate getCategories in admin controller

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -108,15 +108,6 @@ export async function updateProductStatus(req, res, next) {
   }
 }
 
-export async function getCategories(req, res, next) {
-  try {
-    const categories = await adminService.getCategories();
-    res.json({ data: categories });
-  } catch (err) {
-    next(err);
-  }
-}
-
 export async function getKycPending(req, res, next) {
   try {
     const { page, perPage, skip } = req.pagination;

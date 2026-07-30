b7b2b62fc8249651b5141986f323f46fb86c8354
Kodjo AMOUZOU <phipsipy@gmail.com>
2026-07-30 14:28:39 +0000

    Version Moins stable

--- src/app.js
+++ src/app.js
@@ -44,7 +44,6 @@ app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(cors(corsConfig));
 app.use(compression());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(apiLimiter);
 

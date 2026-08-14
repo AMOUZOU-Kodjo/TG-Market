11ea8e1c65a0fc3f87fa7857376977a070dd28e1
AMOUZOU Kodjo <phipsipy@gmail.com>
2026-08-14 08:59:46 +0000

    Modification du nom de la plateforme

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g8g14i8adas"
+    "revision": "0.28b4hp6aq5o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
